f3fb6861bb6a5e7028da87d85fda3bf930073c53
Евгений Камагоров <Kamagorov1991@gmail.com>
2024-05-08 16:46:54 +0600

    first commit

---
+++ src/main/java/ru/lessons2/MyWebAplicattion/MyWebApplication.java
@@ -0,0 +1,14 @@
+package ru.lessons2.MyWebAplicattion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lessons2/MyWebAplicattion/MyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lessons2.MyWebAplicattion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
